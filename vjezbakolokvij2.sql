e98c23ab5708d5dd6e9d26c23dd6b676e65d05c2
Vedran Alagić <59015187+valagic@users.noreply.github.com>
2021-05-21 13:30:13 +0200

    napravljene tablica vj2
    napravljeno 8 tablica vjezba2

---
+++ vjezbakolokvij2.sql
@@ -0,0 +1,71 @@
+drop database if exists vjezba2;
+create database vjezba2;
+use vjezba2;
+
+create table decko (
+	sifra int not null primary key auto_increment,
+	indiferentno bit,
+	vesta varchar(34),
+	asocijalno bit not null
+);
+
+create table prijatelj (
+	sifra int not null primary key auto_increment,
+	modelnaocala varchar(37),
+	treciputa datetime not null,
+	ekstroventno bit not null,
+	prviputa datetime,
+	svekar int not null
+);
+
+create table cura (
+	sifra int not null primary key auto_increment,
+	haljina varchar(33) not null,
+	drugiputa datetime not null,
+	suknja varchar(38),
+	narukvica int,
+	introventno bit,
+	majica varchar(40) not null,
+	decko int
+);
+
+create table decko_zarucnica (
+	sifra int not null primary key auto_increment,
+	decko int not null,
+	zarucnica int not null
+);
+
+create table svekar (
+	sifra int not null primary key auto_increment,
+	stilfrizura varchar(48),
+	ogrlica int not null,
+	asocijalno bit not null
+);
+
+create table zarucnica (
+	sifra int not null primary key auto_increment,
+	narukvica int,
+	bojakosa varchar(37) not null,
+	novcica decimal(15,9),
+	lipa decimal(15,8) not null,
+	indiferentno bit not null
+);
+
+create table brat (
+	sifra int not null primary key auto_increment,
+	suknja varchar(47),
+	ogrlica int not null,
+	asocijalno bit not null,
+	neprijatelj int not null
+);
+
+create table neprijatelj (
+	sifra int not null primary key auto_increment,
+	majica varchar(32),
+	haljina varchar(43) not null,
+	lipa decimal(16,8),
+	modelnaocala varchar(49) not null,
+	kuna decimal(12,6) not null,
+	jmbg char(11),
+	cura int
+);
