976782af361864cd3cad282a67748d388a5f8953
Vedran Alagić <59015187+valagic@users.noreply.github.com>
2021-05-21 13:34:38 +0200

    povezivanje tablica
    napravljen alter table

--- vjezbakolokvij2.sql
+++ vjezbakolokvij2.sql
@@ -69,3 +69,13 @@ create table neprijatelj (
 	jmbg char(11),
 	cura int
 );
+
+alter table cura add foreign key(decko) references decko(sifra);
+alter table decko_zarucnica add foreign key(decko) references decko(sifra);
+alter table prijatelj add foreign key(svekar) references svekar(sifra);
+alter table decko_zarucnica add foreign key(zarucnica) references zarucnica(sifra);
+alter table neprijatelj add foreign key(cura) references cura(sifra);
+alter table brat add foreign key(neprijatelj) references neprijatelj(sifra);
+
+
+
